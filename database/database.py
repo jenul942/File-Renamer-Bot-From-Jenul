2ad745f9c9048064f6ca22af15524190ea96da6c
Jenul Ranthisa <76620261+jenul942@users.noreply.github.com>
2021-04-08 20:54:54 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,7 +1,3 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, scoped_session
-
 import os
 
 import threading
